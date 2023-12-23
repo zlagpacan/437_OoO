be029ba20e9943d69166b3993c767cf375546d7c
zlagpacan <zlagpacan@gmail.com>
2023-12-23 00:35:38 -0500

    fetch_unit_tb.sv init

---
+++ processors/testbench/fetch_unit_tb.sv
@@ -0,0 +1,15 @@
+/*
+    Repo: https://github.com/zlagpacan/437_OoO.git
+    Author: zlagpacan
+
+    File Name: fetch_unit_tb.sv
+    Instantiation Hierarchy: system -> core -> fetch_unit
+    Description: 
+       Testbench for fetch_unit module. 
+*/
+
+module fetch_unit_tb ();
+
+
+
+endmodule
