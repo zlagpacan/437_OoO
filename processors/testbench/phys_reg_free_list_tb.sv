5fe0aae9ebcddea45250f8e49a98f8510f1031e8
Zach Lagpacan <zlagpaca@purdue.edu>
2023-12-29 13:15:29 -0500

    phys_reg_free_list v2 (early running tb based on tb_generator.py)

--- processors/testbench/phys_reg_free_list_tb.sv
+++ processors/testbench/phys_reg_free_list_tb.sv
@@ -36,130 +36,121 @@ module phys_reg_free_list_tb ();
     // DUT signals:
 
     // dequeue
-    // input logic dequeue_valid,
-    logic tb_dequeue_valid;
-    // output phys_reg_tag_t dequeue_phys_reg_tag,
-    phys_reg_tag_t prfl_dequeue_phys_reg_tag, expected_dequeue_phys_reg_tag;
+	logic tb_dequeue_valid;
+	phys_reg_tag_t DUT_dequeue_phys_reg_tag, expected_dequeue_phys_reg_tag;
 
     // enqueue
-    // input logic enqueue_valid,
-    logic tb_enqueue_valid;
-    // input phys_reg_tag_t enqueue_phys_reg_tag,
-    phys_reg_tag_t prfl_enqueue_phys_reg_tag, expected_phys_reg_tag;
+	logic tb_enqueue_valid;
+	phys_reg_tag_t tb_enqueue_phys_reg_tag;
 
     // full/empty
-        // should be left unused by may want to account for potential functionality externally
+        // should be left unused but may want to account for potential functionality externally
         // can use for assertions
-    // output logic full,
-    logic prfl_full, expected_full;
-    // output logic empty,
-    logic prfl_empty, expected_empty;
+	logic DUT_full, expected_full;
+	logic DUT_empty, expected_empty;
 
     // reg map revert
-    // input logic revert_valid,
-    logic tb_revert_valid;
-    // input arch_reg_tag_t revert_dest_arch_reg_tag,
-    // input phys_reg_tag_t revert_safe_dest_phys_reg_tag,
-    // input phys_reg_tag_t revert_speculated_dest_phys_reg_tag,
-    //     // can use for assertion but otherwise don't need
-    phys_reg_tag_t tb_revert_speculated_dest_phys_reg_tag, expected_revert_speculated_dest_phys_reg_tag;
+	logic tb_revert_valid;
+        // input arch_reg_tag_t revert_dest_arch_reg_tag,
+        // input phys_reg_tag_t revert_safe_dest_phys_reg_tag,
+	phys_reg_tag_t tb_revert_speculated_dest_phys_reg_tag;
+        // can use for assertion but otherwise don't need
 
     // free list checkpoint save
-    // input logic save_checkpoint_valid,
-    logic tb_save_checkpoint_valid;
-    // input ROB_index_t save_checkpoint_ROB_index,    // from ROB, write tag val
-    ROB_index_t tb_save_checkpoint_ROB_index;
-    // output checkpoint_column_t save_checkpoint_safe_column,
-    checkpoint_column_t prfl_save_checkpoint_safe_column, expected_save_checkpoint_safe_column;
+	logic tb_save_checkpoint_valid;
+	ROB_index_t tb_save_checkpoint_ROB_index;
+	checkpoint_column_t DUT_save_checkpoint_safe_column, expected_save_checkpoint_safe_column;
 
     // free list checkpoint restore
-    // input logic restore_checkpoint_valid,
-    logic tb_restore_checkpoint_valid;
-    // input logic restore_checkpoint_speculate_failed,
-    logic tb_restore_checkpoint_speculate_failed;
-    // input ROB_index_t restore_checkpoint_ROB_index,  // from restore system, check tag val
-    ROB_index_t tb_restore_checkpoint_ROB_index;
-    // input checkpoint_column_t restore_checkpoint_safe_column,
-    checkpoint_column_t tb_restore_checkpoint_safe_column;
-    // output logic restore_checkpoint_success
-    logic prfl_restore_checkpoint_success, expected_restore_checkpoint_success;
+	logic tb_restore_checkpoint_valid;
+	logic tb_restore_checkpoint_speculate_failed;
+	ROB_index_t tb_restore_checkpoint_ROB_index;
+	checkpoint_column_t tb_restore_checkpoint_safe_column;
+	logic DUT_restore_checkpoint_success, expected_restore_checkpoint_success;
 
     ///////////////////////////////////////////////////////////////////////////////////////////////////////
     // DUT instantiation:
 
-    phys_reg_free_list #(
-        // no params
-    ) DUT (
-        // seq
-        .CLK(CLK),
-        .nRST(nRST),
+	phys_reg_free_list #(
 
-        // dequeue
-        .dequeue_valid(tb_dequeue_valid),
-        .dequeue_phys_reg_tag(prfl_dequeue_phys_reg_tag),
+	) DUT (
+		// seq
+		.CLK(CLK),
+		.nRST(nRST),
 
-        // enqueue
-        .enqueue_valid(tb_enqueue_valid),
-        .enqueue_phys_reg_tag(tb_enqueue_phys_reg_tag),
+	    // dequeue
+		.dequeue_valid(tb_dequeue_valid),
+		.dequeue_phys_reg_tag(DUT_dequeue_phys_reg_tag),
 
-        // full/empty
-            // should be left unused by may want to account for potential functionality externally
-            // can use for assertions
-        .full(prfl_full),
-        .empty(prfl_empty),
+	    // enqueue
+		.enqueue_valid(tb_enqueue_valid),
+		.enqueue_phys_reg_tag(tb_enqueue_phys_reg_tag),
 
-        // reg map revert
-        .revert_valid(tb_revert_valid),
-        // input arch_reg_tag_t revert_dest_arch_reg_tag,
-        // input phys_reg_tag_t revert_safe_dest_phys_reg_tag,
-        .revert_speculated_dest_phys_reg_tag(revert_speculated_dest_phys_reg_tag),
-            // can use for assertion but otherwise don't need
+	    // full/empty
+	        // should be left unused but may want to account for potential functionality externally
+	        // can use for assertions
+		.full(DUT_full),
+		.empty(DUT_empty),
 
-        // free list checkpoint save
-        .save_checkpoint_valid(tb_save_checkpoint_valid),
-        .save_checkpoint_ROB_index(tb_save_checkpoint_ROB_index),    // from ROB, write tag val
-        .save_checkpoint_safe_column(prfl_save_checkpoint_safe_column),
+	    // reg map revert
+		.revert_valid(tb_revert_valid),
+	        // input arch_reg_tag_t revert_dest_arch_reg_tag,
+	        // input phys_reg_tag_t revert_safe_dest_phys_reg_tag,
+		.revert_speculated_dest_phys_reg_tag(tb_revert_speculated_dest_phys_reg_tag),
+	        // can use for assertion but otherwise don't need
 
-        // free list checkpoint restore
-        .restore_checkpoint_valid(tb_restore_checkpoint_valid),
-        .restore_checkpoint_speculate_failed(tb_restore_checkpoint_speculate_failed),
-        .restore_checkpoint_ROB_index(tb_restore_checkpoint_ROB_index),  // from restore system, check tag val
-        .restore_checkpoint_safe_column(tb_restore_checkpoint_safe_column),
-        .restore_checkpoint_success(prfl_restore_checkpoint_success)
-    );
+	    // free list checkpoint save
+		.save_checkpoint_valid(tb_save_checkpoint_valid),
+		.save_checkpoint_ROB_index(tb_save_checkpoint_ROB_index),
+		.save_checkpoint_safe_column(DUT_save_checkpoint_safe_column),
+
+	    // free list checkpoint restore
+		.restore_checkpoint_valid(tb_restore_checkpoint_valid),
+		.restore_checkpoint_speculate_failed(tb_restore_checkpoint_speculate_failed),
+		.restore_checkpoint_ROB_index(tb_restore_checkpoint_ROB_index),
+		.restore_checkpoint_safe_column(tb_restore_checkpoint_safe_column),
+		.restore_checkpoint_success(DUT_restore_checkpoint_success)
+	);
 
     ///////////////////////////////////////////////////////////////////////////////////////////////////////
     // tasks:
 
     task check_outputs();
     begin
-        if (expected_dequeue_phys_reg_tag !== prfl_dequeue_phys_reg_tag) begin
-            $display("\tERROR: expected_dequeue_phys_reg_tag (%h) != prfl_dequeue_phys_reg_tag (%h)", 
-                expected_dequeue_phys_reg_tag, prfl_dequeue_phys_reg_tag);
-            num_errors++;
-            error = 1'b1;
-        end
+		if (expected_dequeue_phys_reg_tag !== DUT_dequeue_phys_reg_tag) begin
+			$display("\tERROR: expected_dequeue_phys_reg_tag (%h) != DUT_dequeue_phys_reg_tag (%h)",
+				expected_dequeue_phys_reg_tag, DUT_dequeue_phys_reg_tag);
+			num_errors++;
+			error = 1'b1;
+		end
 
-        if (expected_full !== prfl_full) begin
-            $display("\tERROR: expected_full (%h) != prfl_full (%h)", 
-                expected_full, prfl_full);
-            num_errors++;
-            error = 1'b1;
-        end
+		if (expected_full !== DUT_full) begin
+			$display("\tERROR: expected_full (%h) != DUT_full (%h)",
+				expected_full, DUT_full);
+			num_errors++;
+			error = 1'b1;
+		end
 
-        if (expected_save_checkpoint_safe_column !== prfl_save_checkpoint_safe_column) begin
-            $display("\tERROR: expected_save_checkpoint_safe_column (%h) != prfl_save_checkpoint_safe_column (%h)",
-                expected_save_checkpoint_safe_column, prfl_save_checkpoint_safe_column);
-            num_errors++;
-            error = 1'b1;
-        end
+		if (expected_empty !== DUT_empty) begin
+			$display("\tERROR: expected_empty (%h) != DUT_empty (%h)",
+				expected_empty, DUT_empty);
+			num_errors++;
+			error = 1'b1;
+		end
 
-        if (expected_restore_checkpoint_success !== prfl_restore_checkpoint_success) begin
-            $display("\tERROR: expected_restore_checkpoint_success (%h) != prfl_restore_checkpoint_success (%h)", 
-                expected_restore_checkpoint_success, prfl_restore_checkpoint_success);
-            num_errors++;
-            error = 1'b1;
-        end
+		if (expected_save_checkpoint_safe_column !== DUT_save_checkpoint_safe_column) begin
+			$display("\tERROR: expected_save_checkpoint_safe_column (%h) != DUT_save_checkpoint_safe_column (%h)",
+				expected_save_checkpoint_safe_column, DUT_save_checkpoint_safe_column);
+			num_errors++;
+			error = 1'b1;
+		end
+
+		if (expected_restore_checkpoint_success !== DUT_restore_checkpoint_success) begin
+			$display("\tERROR: expected_restore_checkpoint_success (%h) != DUT_restore_checkpoint_success (%h)",
+				expected_restore_checkpoint_success, DUT_restore_checkpoint_success);
+			num_errors++;
+			error = 1'b1;
+		end
 
         #(PERIOD / 10);
         error = 1'b0;
@@ -181,10 +172,135 @@ module phys_reg_free_list_tb ();
         sub_test_case = "assert reset";
         $display("\t- sub_test: %s", sub_test_case);
 
-        // reset
-        nRST = 1'b0;
+		// reset
+		nRST = 1'b0;
+	    // dequeue
+		tb_dequeue_valid = 1'b0;
+	    // enqueue
+		tb_enqueue_valid = 1'b0;
+		tb_enqueue_phys_reg_tag = phys_reg_tag_t'(0);
+	    // full/empty
+	    // reg map revert
+		tb_revert_valid = 1'b0;
+		tb_revert_speculated_dest_phys_reg_tag = phys_reg_tag_t'(0);
+	    // free list checkpoint save
+		tb_save_checkpoint_valid = 1'b0;
+		tb_save_checkpoint_ROB_index = ROB_index_t'(0);
+	    // free list checkpoint restore
+		tb_restore_checkpoint_valid = 1'b0;
+		tb_restore_checkpoint_speculate_failed = 1'b0;
+		tb_restore_checkpoint_ROB_index = ROB_index_t'(0);
+		tb_restore_checkpoint_safe_column = checkpoint_column_t'(0);
 
+		@(posedge CLK);
 
+		// outputs:
+
+	    // dequeue
+		expected_dequeue_phys_reg_tag = phys_reg_tag_t'(32);
+	    // enqueue
+	    // full/empty
+		expected_full = 1'b1;
+		expected_empty = 1'b0;
+	    // reg map revert
+	    // free list checkpoint save
+		expected_save_checkpoint_safe_column = checkpoint_column_t'(0);
+	    // free list checkpoint restore
+		expected_restore_checkpoint_success = 1'b0;
+
+		check_outputs();
+
+        // inputs:
+        sub_test_case = "deassert reset";
+        $display("\t- sub_test: %s", sub_test_case);
+
+		// reset
+		nRST = 1'b1;
+	    // dequeue
+		tb_dequeue_valid = 1'b0;
+	    // enqueue
+		tb_enqueue_valid = 1'b0;
+		tb_enqueue_phys_reg_tag = phys_reg_tag_t'(0);
+	    // full/empty
+	    // reg map revert
+		tb_revert_valid = 1'b0;
+		tb_revert_speculated_dest_phys_reg_tag = phys_reg_tag_t'(0);
+	    // free list checkpoint save
+		tb_save_checkpoint_valid = 1'b0;
+		tb_save_checkpoint_ROB_index = ROB_index_t'(0);
+	    // free list checkpoint restore
+		tb_restore_checkpoint_valid = 1'b0;
+		tb_restore_checkpoint_speculate_failed = 1'b0;
+		tb_restore_checkpoint_ROB_index = ROB_index_t'(0);
+		tb_restore_checkpoint_safe_column = checkpoint_column_t'(0);
+
+		@(posedge CLK);
+
+		// outputs:
+
+	    // dequeue
+		expected_dequeue_phys_reg_tag = phys_reg_tag_t'(32);
+	    // enqueue
+	    // full/empty
+		expected_full = 1'b1;
+		expected_empty = 1'b0;
+	    // reg map revert
+	    // free list checkpoint save
+		expected_save_checkpoint_safe_column = checkpoint_column_t'(0);
+	    // free list checkpoint restore
+		expected_restore_checkpoint_success = 1'b0;
+
+		check_outputs();
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////
+        // default:
+        test_case = "default";
+        $display("\ntest %d: %s", test_num, test_case);
+        test_num++;
+
+		@(posedge CLK);
+
+		// inputs
+		sub_test_case = "default";
+		$display("\t- sub_test: %s", sub_test_case);
+
+		// reset
+		nRST = 1'b1;
+	    // dequeue
+		tb_dequeue_valid = 1'b0;
+	    // enqueue
+		tb_enqueue_valid = 1'b0;
+		tb_enqueue_phys_reg_tag = phys_reg_tag_t'(0);
+	    // full/empty
+	    // reg map revert
+		tb_revert_valid = 1'b0;
+		tb_revert_speculated_dest_phys_reg_tag = phys_reg_tag_t'(0);
+	    // free list checkpoint save
+		tb_save_checkpoint_valid = 1'b0;
+		tb_save_checkpoint_ROB_index = ROB_index_t'(0);
+	    // free list checkpoint restore
+		tb_restore_checkpoint_valid = 1'b0;
+		tb_restore_checkpoint_speculate_failed = 1'b0;
+		tb_restore_checkpoint_ROB_index = ROB_index_t'(0);
+		tb_restore_checkpoint_safe_column = checkpoint_column_t'(0);
+
+		@(negedge CLK);
+
+		// outputs:
+
+	    // dequeue
+		expected_dequeue_phys_reg_tag = phys_reg_tag_t'(32);
+	    // enqueue
+	    // full/empty
+		expected_full = 1'b1;
+		expected_empty = 1'b0;
+	    // reg map revert
+	    // free list checkpoint save
+		expected_save_checkpoint_safe_column = checkpoint_column_t'(0);
+	    // free list checkpoint restore
+		expected_restore_checkpoint_success = 1'b0;
+
+		check_outputs();
 
         ///////////////////////////////////////////////////////////////////////////////////////////////////
         // finish:
@@ -209,3 +325,4 @@ module phys_reg_free_list_tb ();
     end
 
 endmodule
+
