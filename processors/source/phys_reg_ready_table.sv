445d8b0e61da939725d9f0a507db65de54b21dbf
Zach Lagpacan <zlagpaca@purdue.edu>
2024-01-02 12:16:50 -0500

    phys_reg_ready_table v2 (finished tb)

--- processors/source/phys_reg_ready_table.sv
+++ processors/source/phys_reg_ready_table.sv
@@ -9,6 +9,8 @@
         be computed in the datapath. As far as can tell, this module does not require checkpointing or 
         revert logic since future instr's that use a value will use the most recent dispatch's ready val,
         which may or may not have been completed since. 
+        
+        It is the external controller's responsibility to prevent the clearing of phys reg 0.
 */
 
 `include "core_types.vh"
