82003e99c41e66dfb82c4a132868685f9e9fbae8
Zachary Lagpacan <zlagpaca@purdue.edu>
2024-05-12 00:05:14 -0400

    more test cases, more debug

--- processors/asmFiles/multi.shared.read.asm
+++ processors/asmFiles/multi.shared.read.asm
@@ -1,4 +1,16 @@
 org 0x0000
 
+lw $t0, 0x1000($zero)
+addi $t0, $t0, 1
+sw $t0, 0x2000($zero)
+halt
 
 org 0x0200
+
+lw $t0, 0x1000($zero)
+addi $t0, $t0, 2
+sw $t0, 0x3000($zero)
+halt
+
+org 0x1000
+cfw 0x0123
